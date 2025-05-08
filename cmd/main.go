df8c1a5b123190f8a3ae0375c4ad31a3b84fdcf3
Ahmad Fauzi <adzi.fauziahmad@gmail.com>
2025-05-08 19:06:00 +0700

    apply rate limiter

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/adzi007/ecommerce-order-service/cmd/server"
 	"github.com/adzi007/ecommerce-order-service/config"
 	"github.com/adzi007/ecommerce-order-service/internal/infrastructure/database"
@@ -8,6 +10,8 @@ import (
 	"github.com/adzi007/ecommerce-order-service/internal/infrastructure/logger"
 	"github.com/adzi007/ecommerce-order-service/internal/infrastructure/monitoring"
 	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func main() {
@@ -25,6 +29,19 @@ func main() {
 	defer conn.Close()
 
 	servernya := server.NewFiberServer(db, conn)
+
+	servernya.Use(limiter.New(limiter.Config{
+		Max:        10,               // 10 requests
+		Expiration: 30 * time.Second, // per 30 seconds
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP() // limit per IP
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error": "Too many requests",
+			})
+		},
+	}))
 
 	// Register Prometheus metrics
 	monitoring.RegisterMetrics()
--- go.mod
+++ go.mod
@@ -39,6 +39,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
@@ -50,6 +51,7 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.58.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
